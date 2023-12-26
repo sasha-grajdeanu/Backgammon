7a4ca082c81ff57162b22c53dd4fe1876181c797
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-12-26 18:52:07 +0200

    testing functii

--- main.py
+++ main.py
@@ -12,18 +12,10 @@ def main():
     game = backgammon.Backgammon()
     # print(game)
     # print(fortune.dices())
-    game.tabla[0] = 2
-    game.tabla[1] = -2
-    game.tabla[2] = 0
-    game.tabla[3] = -1
-    game.tabla[4] = 0
-    game.tabla[5] = 2
-
-    game.remove_black = 2
 
     print(game)
 
-    move_player_black.move_black(game)
+    move_player_white.move_white(game)
 
     print(game)
 
