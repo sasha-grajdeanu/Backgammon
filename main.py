0e6dc903cad5c9017afb6e17e406378cfa2de671
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-12-26 12:07:20 +0200

    Testare

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import math_moves
 import fortune
 import backgammon
 import move_piece
+import move_player_white
+import move_player_black
 
 
 def main():
     game = backgammon.Backgammon()
+    # print(game)
+    # print(fortune.dices())
+    game.tabla[0] = 2
+    game.tabla[1] = -2
+    game.tabla[2] = 0
+    game.tabla[3] = -1
+    game.tabla[4] = 0
+    game.tabla[5] = 2
+
+    game.remove_black = 2
+
     print(game)
-    print(fortune.dices())
-    print(math_moves.available_move(game, game.white, 3, 4))
-    print(move_piece.move_piece(game, game.white, math_moves.available_move(game, game.white, 3, 4), 5, 2))
-    print(move_piece.move_piece(game, game.white, math_moves.available_move(game, game.white, 3, 4), 5, 1))
+
+    move_player_black.move_black(game)
+
     print(game)
-    print(game.can_remove_piece(game.white))
-    print(fortune.decides_who_start())
+
+
 if __name__ == "__main__":
     main()
