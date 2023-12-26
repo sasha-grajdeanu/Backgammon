431abc296e1e5f12e198963babc41ebcf147bf93
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-12-26 19:17:24 +0200

    testing la scos piese

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ def main():
     # print(fortune.dices())
 
     print(game)
+    game.tabla[0] = 0
+    game.tabla[11] = 0
+    game.tabla[16] = 0
 
-    move_player_white.move_white(game)
+    game.tabla[18] = 0
+    game.tabla[19] = 0
+    game.tabla[20] = -3
+    game.tabla[21] = -5
+    game.tabla[22] = -2
+    game.tabla[23] = -5
+
+    move_player_black.move_black(game)
 
     print(game)
 
