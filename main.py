e6e662d92da13e214318c208327acd72aafd1b15
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-12-28 22:25:28 +0200

    testing

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ def main():
     game.tabla[12] = 0
     game.tabla[7] = 0
 
-    game.tabla[5] = 1
-    game.tabla[4] = 1
-    game.tabla[3] = 2
+    game.tabla[5] = 0
+    game.tabla[4] = 0
+    game.tabla[3] = 1
     game.tabla[2] = 1
     game.tabla[1] = 5
-    game.tabla[0] = 5
+    game.tabla[0] = 6
 
     print(game)
 
-    print(math_moves.available_move(game, game.white, 5, 5, 4))
-    # move_player_black.move_black(game)
+    print(math_moves.available_move(game, game.white, 5, 5, 1))
+    # move_player_white.move_white(game)
     # for i in range(0, 6).__reversed__():
     #     print(i)
 
