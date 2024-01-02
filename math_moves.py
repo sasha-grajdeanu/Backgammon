0262a55572178d29db809bb1792c9ae9ed3eacbd
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2024-01-02 19:57:21 +0200

    refacut mod de calcul + reparat bug

--- math_moves.py
+++ math_moves.py
@@ -7,14 +7,14 @@ def available_move(situation: backgammon.Backgammon, player, dice_1, dice_2, dou
     list_of_possible_moves = dict()
     if player == situation.black:
         if situation.can_remove_piece(situation.black):
-            for i in range(0, max(dice_1, dice_2) + 1).__reversed__():
-                # print("hey")
-                if 24 - i == dice_1 and situation.tabla[i] >= 0:
-                    dice_1 -= 1
-                if 24 - i == dice_2 and situation.tabla[i] >= 0:
-                    dice_2 -= 1
-                if situation.tabla[i] > 0:
+            for i in range(18, 24):
+                if situation.tabla[i] < 0:
                     break
+                else:
+                    if dice_1 == 24 - i:
+                        dice_1 -= 1
+                    if dice_2 == 24 - i:
+                        dice_2 -= 1
         for i in range(len(situation.tabla)):
             # print(dice_1, dice_2)
             if situation.tabla[i] < 0:
@@ -22,14 +22,16 @@ def available_move(situation: backgammon.Backgammon, player, dice_1, dice_2, dou
                 if double is None:
                     list_of_movement.add(i + dice_1)
                     list_of_movement.add(i + dice_2)
-                    list_of_movement.add(i + dice_1 + dice_2)
+                    # list_of_movement.add(i + dice_1 + dice_2)
                     if i in list_of_movement:
                         list_of_movement.remove(i)
                 else:
                     if double is None:
                         return list_of_possible_moves
                     for j in range(double):
-                        list_of_movement.add(i + (j + 1) * dice_1)
+                        list_of_movement.add(i + dice_1)
+                    if i in list_of_movement:
+                        list_of_movement.remove(i)
                 flag = False
                 list_of_movement = list(list_of_movement)
                 for element in deepcopy(list_of_movement):
@@ -49,17 +51,18 @@ def available_move(situation: backgammon.Backgammon, player, dice_1, dice_2, dou
                             list_of_movement.remove(element)
                 if flag:
                     list_of_movement.append(-1)
-                list_of_possible_moves[i] = list_of_movement
+                if len(list_of_movement) != 0:
+                    list_of_possible_moves[i] = list_of_movement
     if player == situation.white:
         if situation.can_remove_piece(situation.white):
-            for i in range(0, max(dice_1, dice_2) + 1).__reversed__():
-                # print("hey", i)
-                if i + 1 == dice_1 and situation.tabla[i] <= 0:
-                    dice_1 -= 1
-                if i + 1 == dice_2 and situation.tabla[i] <= 0:
-                    dice_2 -= 1
+            for i in range(0, 6).__reversed__():
                 if situation.tabla[i] > 0:
                     break
+                else:
+                    if dice_1 == i + 1:
+                        dice_1 -= 1
+                    if dice_2 == i + 1:
+                        dice_2 -= 1
 
         for i in range(len(situation.tabla)).__reversed__():
             # print(dice_1, dice_2)
@@ -68,12 +71,14 @@ def available_move(situation: backgammon.Backgammon, player, dice_1, dice_2, dou
                 if double is None:
                     list_of_movement.add(i - dice_1)
                     list_of_movement.add(i - dice_2)
-                    list_of_movement.add(i - dice_1 - dice_2)
+                    # list_of_movement.add(i - dice_1 - dice_2)
                     if i in list_of_movement:
                         list_of_movement.remove(i)
                 else:
                     for j in range(double):
-                        list_of_movement.add(i - (j + 1) * dice_1)
+                        list_of_movement.add(i - dice_1)
+                    if i in list_of_movement:
+                        list_of_movement.remove(i)
 
                 flag = False
                 list_of_movement = list(list_of_movement)
@@ -94,26 +99,27 @@ def available_move(situation: backgammon.Backgammon, player, dice_1, dice_2, dou
                             list_of_movement.remove(element)
                 if flag:
                     list_of_movement.append(-1)
-                list_of_possible_moves[i] = list_of_movement
+                if len(list_of_movement) != 0:
+                    list_of_possible_moves[i] = list_of_movement
     return list_of_possible_moves
 
 
 def where_can_place_piece(situation: backgammon.Backgammon, player, dice_1, dice_2):
-    list_of_possible_place = list()
+    list_of_possible_place = set()
     if player == situation.white:
-        list_of_possible_place.append(24 - dice_1)
-        list_of_possible_place.append(24 - dice_2)
+        list_of_possible_place.add(24 - dice_1)
+        list_of_possible_place.add(24 - dice_2)
         for elem in deepcopy(list_of_possible_place):
             if elem == 24:
                 list_of_possible_place.remove(elem)
             elif situation.tabla[elem] < -1:
                 list_of_possible_place.remove(elem)
     if player == situation.black:
-        list_of_possible_place.append(-1 + dice_1)
-        list_of_possible_place.append(-1 + dice_2)
+        list_of_possible_place.add(-1 + dice_1)
+        list_of_possible_place.add(-1 + dice_2)
         for elem in deepcopy(list_of_possible_place):
-            if elem == 0:
+            if elem == -1:
                 list_of_possible_place.remove(elem)
             elif situation.tabla[elem] > 1:
                 list_of_possible_place.remove(elem)
-    return list_of_possible_place
+    return list(list_of_possible_place)
