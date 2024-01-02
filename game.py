42f1e7254f5347219db231b23dbe2047e8f13be2
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2024-01-02 22:16:04 +0200

    facut functie jucat om vs ai

--- game.py
+++ game.py
@@ -2,9 +2,11 @@ import fortune
 import backgammon
 import move_player_white
 import move_player_black
+import moves_ai_player_black
+import move_ai_player_white
 
 
-def game():
+def game_h_vs_h():
     game = backgammon.Backgammon()
     turn = 0
     start_player = fortune.decides_who_start()
@@ -28,4 +30,41 @@ def game():
             else:
                 print("WHITE TURN")
                 move_player_white.move_white(game)
+                turn = -1
+
+def game_h_vs_ai():
+    game = backgammon.Backgammon()
+    establish_ai = fortune.decides_who_start()
+    if establish_ai:
+        print("AI = BLACK")
+    else:
+        print("AI = WHITE")
+    turn = 0
+    start_player = fortune.decides_who_start()
+    if start_player:
+        turn = -1
+    else:
+        turn = 1
+    while True:
+        # print(game)
+        if game.win_what() == 1:
+            print("White win")
+            break
+        if game.win_what() == -1:
+            print("Black win")
+            break
+        else:
+            if turn == -1:
+                print("BLACK TURN")
+                if establish_ai:
+                    moves_ai_player_black.move_ai_black(game)
+                else:
+                    move_player_black.move_black(game)
+                turn = 1
+            else:
+                print("WHITE TURN")
+                if not establish_ai:
+                    move_ai_player_white.move_ai_white(game)
+                else:
+                    move_player_white.move_white(game)
                 turn = -1
