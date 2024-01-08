3473a914e627b1e146c39b8b55577ccc3c7c9468
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2024-01-08 16:14:31 +0200

    Add documentation for each method + refactor
    Put this class in a logic_of_game director

--- logic_of_game/backgammon.py
+++ logic_of_game/backgammon.py
@@ -1,7 +1,8 @@
 class Backgammon:
     def __init__(self):
-        """initialization of game with default variables
-           tabla = represent the real gameboard with 24 columns
+        """
+        initialization of game with default variables
+           tabla = represent the real game board with 24 columns
            remove_color (where color is black or white) represent the pieces removed by opponent
            color_set (where color is black or white) represent the pieces removed by the player with the same color
         """
@@ -23,6 +24,10 @@ class Backgammon:
         self.black = -1
 
     def reset(self):
+        """
+        method to reset the game
+        :return: nothing
+        """
         self.tabla = list(0 for i in range(24))
         self.tabla[23] = 2
         self.tabla[18] = -5
@@ -38,10 +43,12 @@ class Backgammon:
         self.black_set = 0
         self.white = 1
         self.black = -1
+
     def __str__(self):
-
-        """overwriting __str__ method, adapt for my class"""
-
+        """
+        method to print the class
+        :return: string representation of the class
+        """
         up = self.tabla[:12]
         up.reverse()
         down = self.tabla[12:]
@@ -50,9 +57,11 @@ class Backgammon:
             self.remove_white) + "\n" + "Scoase negre = " + str(self.remove_black)
 
     def can_remove_piece(self, player):
-
-        """method to check if a player can remove pieces """
-
+        """
+        checks if the player can remove the piece on the board
+        :param player:
+        :return: False when the player cannot remove the piece, True otherwise
+        """
         if player == self.white:
             for i in range(6, 24):
                 if self.tabla[i] > 0:
@@ -68,8 +77,13 @@ class Backgammon:
                 return False
             return True
 
-
     def win_what(self):
+        """
+        method for:
+            1. checking the game status (if it's finished or not)
+            2. determining the winner
+        :return: 1 if white win, -1 if black win, False otherwise
+        """
         if self.white_set == 15:
             return 1
         if self.black_set == 15:
